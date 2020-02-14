0b647a65d47d85184746e45b87f86ff7a082a289
chendapao <feloxx@163.com>
2020-02-14 18:16:39 +0800

    add cluster doc.

--- controllers/cluster.go
+++ controllers/cluster.go
@@ -1,2 +1,37 @@
 package controllers
 
+import "github.com/astaxie/beego"
+
+type ClusterController struct {
+	beego.Controller
+}
+
+// 创建/更新集群
+func (c *ClusterController) CreateUpdateCluster() {
+
+}
+
+// 删除集群
+func (c *ClusterController) DeleteCluster() {
+
+}
+
+// 查询集群列表
+func (c *ClusterController) SelectClusterList() {
+
+}
+
+// 执行集群
+func (c *ClusterController) ExecuteCluster() {
+
+}
+
+// 查看上一次执行日志
+func (c *ClusterController) ReadLog() {
+
+}
+
+// 执行结果信号
+func (c *ClusterController) ExecuteResultSignal() {
+
+}
--- models/cluster.go
+++ models/cluster.go
@@ -1,10 +1,57 @@
 package models
 
+// 基础信息
+type BaseInfo struct {
+	Version    string `json:"version"`    // ds版本
+	DeployUser string `json:"deployUser"` //ds部署用户
+	DeployPath string `json:"deployPath"` //ds部署路径
+}
+
+// 角色信息
+type RoleInfo struct {
+	RoleName     string     `json:"name"`
+	RoleBody     ConfigBody `json:"roleBody"`
+	DependHostId int        `json:"dependHostId"`
+}
+
+// 集群状态
+type ClusterType int
+
+const (
+	Stopped = iota //停止
+	Started        //启动
+)
+
+// 集群信息
 type ClusterInfo struct {
-  Name      string `json:"name"`  // 集群名称
-  Model     string `json:"model"` // 集群模式
-  Status    interface{}           // 当前状态
-  HostInfos []HostInfo            // 主机信息
-  BaseInfo  interface{}           // 基本信息
-  RoleInfos interface{}           // 角色信息
+	Id     int         `json:"id"`     // 集群id
+	Name   string      `json:"name"`   // 集群名称
+	Status ClusterType `json:"status"` // 当前状态
+	Hosts  []int       `json:"hosts"`  // 主机信息
+	Base   BaseInfo    `json:"base"`   // 基本信息
+	Roles  []RoleInfo  `json:"roles"`  // 角色信息
+}
+
+// 执行类型
+type ExecuteType int
+
+const (
+	Stop = iota
+	Start
+	DeployUpdate
+)
+
+// 执行结果
+type ExecuteResultType int
+
+const (
+	Success = iota
+	Failed
+)
+
+// 执行请求
+type ExecuteInfo struct {
+	ExecuteType   ExecuteType       `json:"executeType"`
+	ClusterId     int               `json:"clusterId"`
+	ExecuteResult ExecuteResultType `json:"executeResult"`
 }
